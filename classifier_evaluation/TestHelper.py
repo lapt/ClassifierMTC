3553d33d504e170027c80cd236dcead49cff45e9
LuisAngel <lapt.paris@gmail.com>
2020-12-26 06:28:58 -0500

    Update service

--- classifier_evaluation/TestHelper.py
+++ classifier_evaluation/TestHelper.py
@@ -33,31 +33,32 @@ def test_model(model, X_train, y_train, X_test, y_test, ngram_range, use_idf):
     result = metrics.classification_report(y_test, predicted)
     print(result)
     print(metrics.confusion_matrix(y_test, predicted))
-    # df_result.to_csv(path_result, index=False, header=True, encoding='utf-8', sep="\t")
     return text_cls
 
 
 def evaluation(path_corpus_train, path_corpus_test):
     subject_corpus_train = create_subject_corpus(path_corpus_train)
+
     msg_train = subject_corpus_train['subject']
     label_train = subject_corpus_train['priority']
     subject_corpus_test = create_subject_corpus(path_corpus_test)
     msg_test = subject_corpus_test['subject']
     label_test = subject_corpus_test['priority']
-    # print("RandonForest")
-    # model = RandomForestClassifier(criterion='gini', max_features='log2', n_estimators=150)
-    # model_predict = test_model(model, msg_train, label_train, msg_test, label_test, (1, 1), False)
-    # joblib.dump(model_predict, "results_models_test/RandonForest.joblib")
-    #
-    # print("LinerSVC")
-    # model = svm.LinearSVC(C=2, loss='squared_hinge')
-    # model_predict = test_model(model, msg_train, label_train, msg_test, label_test, (1, 2), True)
-    # joblib.dump(model_predict, "results_models_test/LinerSVC.joblib")
-    #
-    # print("MultinomialNB")
-    # model = MultinomialNB(alpha=0.01)
-    # model_predict = test_model(model, msg_train, label_train, msg_test, label_test, (1, 2), False)
-    # joblib.dump(model_predict, "results_models_test/MultinomialNB.joblib")
+
+    print("RandonForest")
+    model = RandomForestClassifier(criterion='gini', max_features='log2', n_estimators=150)
+    model_predict = test_model(model, msg_train, label_train, msg_test, label_test, (1, 1), False)
+    joblib.dump(model_predict, "results_models_test/RandonForest.joblib")
+
+    print("LinerSVC")
+    model = svm.LinearSVC(C=2, loss='squared_hinge')
+    model_predict = test_model(model, msg_train, label_train, msg_test, label_test, (1, 2), True)
+    joblib.dump(model_predict, "results_models_test/LinerSVC.joblib")
+
+    print("MultinomialNB")
+    model = MultinomialNB(alpha=0.01)
+    model_predict = test_model(model, msg_train, label_train, msg_test, label_test, (1, 2), False)
+    joblib.dump(model_predict, "results_models_test/MultinomialNB.joblib")
 
     print("SVC")
     model = svm.SVC(C=100, kernel='rbf', probability=True)
